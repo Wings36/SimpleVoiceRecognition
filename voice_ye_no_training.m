3d47531d640b2df2cf91a57ed1ab444ff809b91b
BuildTools <unconfigured@null.spigotmc.org>
2022-03-07 18:40:48 -0800

    Imported audio file

--- voice_ye_no_training.m
+++ voice_ye_no_training.m
@@ -1,1 +1,16 @@
 clear all;clc;close all; 
+[samples,sampleRate] = audioread("train\no\0.wav");
+windowSize = 1024;
+numOfWindows = samples / windowSize;
+numOfWindows = floor(numOfWindows);
+samplePointer = 1;
+
+test = mat2cell(samples, windowSize);
+
+
+% for x = 1:numOfWindows
+%     
+%     
+% 
+% 
+% end
