d92c46e6b55210178add696938c50ad4f82d4f9b
Peter Hoang <peterhg7@gmail.com>
2022-03-08 15:23:15 -0800

    Fixed a bug where number of windows was an array instead of a int value

--- voice_ye_no_training.m
+++ voice_ye_no_training.m
@@ -1,20 +1,21 @@
 clear all;clc;close all; 
 [samples,sampleRate] = audioread("train\no\0.wav");
 windowSize = 1024;
-numOfWindows = samples / windowSize;
+numOfWindows = size(samples) / windowSize;
 numOfWindows = floor(numOfWindows);
+numOfWindows = numOfWindows(1,1);
 samplePointer = 1;
 % PSD = 20*log10(abs()); %PSD function
 
 
-for x = 1:1
+for x = 1:numOfWindows
     windowSelect = samples(samplePointer:windowSize);
     windowFFT = fftshift(fft(windowSelect));
     magWindow = abs(windowFFT);
 
     %Converted to dB
     magWindow = 20 * log10(abs(magWindow));
-
+    
     %discard bins corresponding to negative frequency components
     
 end
