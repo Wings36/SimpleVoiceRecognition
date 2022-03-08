a5f04ddec83129fdecf50327b6126315d6d8f33f
tonyk <tonyk@192.168.86.29>
2022-03-08 11:03:58 -0800

    Added a function to test if it is yes or no

--- voice_ye_no_testing.m
+++ voice_ye_no_testing.m
@@ -1,1 +1,16 @@
 clear all;clc;close all; 
+function result = voice_recognition(x, Fs)
+%threshold = ?; threshold has to be determined based on lo/hi ratio
+
+N = length(x);
+low = round(N*4000/Fs);
+high = round(N*8000/Fs);
+
+shiftx = abs(fft(x));
+feature = sum(shiftx(1:low))/sum(shiftx(low:high));
+
+if featrue < threshold
+    result = 'yes';
+else
+    result = 'no';
+end 
